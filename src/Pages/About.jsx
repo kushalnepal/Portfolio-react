e462766dfce9a5c23c91784cb3ae6beb59fccb21
kushalnepal <nepalkushal74@gmail.com>
2025-01-11 18:37:15 -0800

    bug-fix

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -215,7 +215,7 @@ const AboutPage = () => {
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/file/d/10NUOoHbCptXdxK8yqbo0Ctk4DuA8w8Qd/view?usp=sharing" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/1VRf6gCy6mqmR9LVK69AQCQGBeogurI-r/view?usp=sharing" className="w-full lg:w-auto">
                 <button 
                   data-aos="fade-up"
                   data-aos-duration="800"
